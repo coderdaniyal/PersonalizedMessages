750ce3c51193f1a8d45e9f5b4d2399b598330d8a
codewithdaniyal <167603648+codewithdaniyal@users.noreply.github.com>
2024-06-04 18:18:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var myFriends = ["Farhan", "Naveed", "Musa"];
+myFriends.forEach(function (name) {
+    console.log("Hello, ".concat(name, "! Hope you are having a great day!"));
+});
---
+++ app.ts
@@ -0,0 +1,4 @@
+let myFriends:string[] = ["Farhan", "Naveed", "Musa"]
+myFriends.forEach(name => {
+    console.log(`Hello, ${name}! Hope you are having a great day!`);
+});
